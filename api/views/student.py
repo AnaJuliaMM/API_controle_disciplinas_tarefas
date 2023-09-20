04f14aa11f8e021fa231f8a3e23ca9392592b76c
AnaJuliaMM <ana.martins_2004@outlook.com>
2023-09-20 20:37:08 -0300

    doc(student-view): add comments to the method

--- api/views/student.py
+++ api/views/student.py
@@ -17,7 +17,9 @@ class StudentView(APIView):
         try:
             #Get the data from the request and serialize it
             serializer = StudentSerializer(data=request.data)
+            #Validete the data serialized
             serializer.is_valid(raise_exception=True)
+            #Save the new object in the database
             serializer.save()
             return Response({"message": "Student created sucessfully", "data": serializer.data }, status=status.HTTP_201_CREATED)
         except:
@@ -29,8 +31,11 @@ class StudentView(APIView):
             Method receives a http POST request and format type and return all students in the database
         """
         try:
+            #Get all students objects 
             students = StudentModel.objects.all()
+            #Serializer the objects
             serializer = StudentSerializer(students, many=True)
+            # Return the objects serialized and a sucess message
             return Response({"message": "All students returned", "data": serializer.data }, status=status.HTTP_200_OK)
         except:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
