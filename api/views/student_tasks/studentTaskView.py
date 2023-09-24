953803ead4b30f737d78030463bb8128bf20d9fa
Ana Julia M Martins <ana_juh17@hotmail.com>
2023-09-24 10:57:29 -0300

    feat(view): added 404 not found exception message

--- api/views/student_tasks/studentTaskView.py
+++ api/views/student_tasks/studentTaskView.py
@@ -3,6 +3,12 @@ from rest_framework.response import Response
 from rest_framework import status
 from api.models.task import TaskModel
 from api.serializers.task import TaskSerializer
+from django.shortcuts import get_object_or_404
+from django.http import Http404
+from api.models.student import StudentModel
+
+
+
 
 
 class StudentTaskView(APIView):
@@ -10,13 +16,26 @@ class StudentTaskView(APIView):
     List all tasks for a given student.
     """
     def get(self, request, pk, format=None):
-        try: 
-            #Gets all tasks objects 
+        """
+            Returns a JSON of the object that contains the primary key handled by the url  
+
+            Args:
+            pk(int)  a value that represents the object pk
+
+        """
+        try:
+            #Verifies if the student exists
+            get_object_or_404(StudentModel, pk=pk) 
+            #Gets all tasks objects related to the student
             tasks = TaskModel.objects.filter(student = pk)
             #Serializes the objects
             serializer = TaskSerializer(tasks, many=True)
             #Returns the tasks found
             return Response({"message": "All tasks returned", "data": serializer.data }, status=status.HTTP_200_OK)
+        #Catches a exception raised in case of the student does not exist
+        except Http404:
+            #Retuns a error message with the error explanation 
+            return Response({"message": "Student not found"}, status=status.HTTP_404_NOT_FOUND)
         #Abstracts all exception through python Exception class
         except Exception as e:
             #Retuns a error message with the error explanation 
